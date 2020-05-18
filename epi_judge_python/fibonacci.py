f9c1b7441ec14661e9cc0916dd401fe42103c726
Vinicius Agostini <vinicius.agostini19@gmail.com>
2020-05-18 01:10:40 -0300

    update
    update

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
 
 
-def fibonacci(n: int) -> int:
-    # TODO - you fill in here.
-    return -1
+def fibonacci(n, cache={}):
+    if n <= 1:
+        return n
+    
+    if n not in cache:
+        cache[n] = fibonacci(n-1) + fibonacci(n-2)
+
+    return cache[n]
+
+import functools
+
+@functools.lru_cache(None)
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    return fibonacci(n-1) + fibonacci(n-2)
 
 
 if __name__ == '__main__':
